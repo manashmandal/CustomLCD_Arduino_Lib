51262295554786f885b5934d0957c3fd2018912c
Manash Mandal <manashmndl@gmail.com>
2015-05-21 01:34:05 +0600

    first commit

--- CustomCrystalDisplay.h
+++ CustomCrystalDisplay.h
@@ -2,17 +2,23 @@
 #define CustomCrystalDisplay_h
 
 #include <Arduino.h>
-#include <LiquidCrystal\src\LiquidCrystal.h>
+#include "LiquidCrystal.h"
 
 class CustomLCD : public LiquidCrystal
 {
 
 private:
 	
+	//Initial Positions
+	uint8_t x = 0 , y = 0;
+	uint8_t scroll_delay;
+
 	//Storage for row and column
 	 uint8_t lcdRows;
 	 uint8_t lcdCols;
 	 int wordlength;
+	 //Distance from first character to the last boundary of lcd
+	 uint8_t distance;
 
 public:
 	//Function for scrolling from left to right 
@@ -30,6 +36,11 @@ public:
 		
 		: LiquidCrystal(a, b, c, d, e, f) {}
 
+	//Calculate distance from first character of the string to the last LCD boundary
+	uint8_t get_distance();
+
+	//bool transfer(char * string);
+	void transfer(char *string);
 
 };
 
--- CustomLiquidCrystal.cpp
+++ CustomLiquidCrystal.cpp
@@ -1,5 +1,6 @@
 #include "CustomCrystalDisplay.h"
 
+
 void CustomLCD::setLCD(uint8_t col, uint8_t row)
 {
 	lcdRows = row;
@@ -16,51 +17,61 @@ uint8_t CustomLCD::getRow()
 	return lcdRows;
 }
 
+uint8_t CustomLCD::get_distance()
+{
+	return getCol() + 1 - CustomLCD::x;
+}
 
+void CustomLCD::transfer(char *string)
+{
+	int xPos = x;
+	while (x < getCol())
+	{
+		clear();
+		for  (int index = 0, xPos = x; xPos < getCol(); xPos++, index++){
+			setCursor(xPos, y);
+			print(string[index]);
+		}
+		delay(scroll_delay);
+		clear();
+		x++;
+	}
+}
 
 void CustomLCD::word_scroll(char *str, uint8_t del)
 {
 	//String Length
 	wordlength = strlen(str);
+	scroll_delay = del;
 
 	//Getting row and column for boundary calculation
 	uint8_t max_col = getCol();
 	uint8_t max_row = getRow();
 
 	//Initial Positions
-	uint8_t x = 0;
-	uint8_t y = 0;
-
-	int iterator = 0;
+	 CustomLCD::x = 0;
+	 CustomLCD::y = 0;
 
 	//The block that actually do the scrolling
 	while (true){
-
-		iterator = x + wordlength;
-
-		if (iterator <= max_col){
+		if (get_distance() >= wordlength){
+			//print("Func called");
 			setCursor(x, y);
-			LiquidCrystal::print(str);
+			print(str);
 			delay(del);
-			LiquidCrystal::clear();
-
-			if (iterator == max_col){
-				y++;
-				x = -1;  
-				
-
-			}
-
+			clear();
 			x++;
-		}
 
+			if (get_distance() == wordlength){
+				transfer(str);
+				y++;
+				x = 0;
+			}
+		}
+		//If row goes unbounded this codeblock resets the coordinates
 		if (y >= max_row) {
 			y = 0;
 			x = 0;
 		}
-		
 	}
-	
-	
-
 }
